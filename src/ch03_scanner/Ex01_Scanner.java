86bde38963d25afaba57b035b1b4b13759114926
hippi-hamin <9908haha@naver.com>
2023-12-01 11:32:24 +0900

    스캐너 적용해보기

--- src/ch03_scanner/Ex01_Scanner.java
+++ src/ch03_scanner/Ex01_Scanner.java
@@ -1,10 +1,18 @@
 package ch03_scanner;
 
+// java.util 패키지에 소속된 Scanner 클래스를 가져옴.
+import java.util.Scanner;
+
 public class Ex01_Scanner {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// scanner 라는 이름의 Scanner 클래스 객체 선언
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("숫자를 입력하세요 : "); //println()에서 ln의 의미는 ()를 출력하고 줄을 바꿈
+		int num1 = scanner.nextInt();
+		System.out.println(num1);
+		
+		
 	}
 
 }
---
+++ src/ch03_scanner/Ex02_Calculator.java
@@ -0,0 +1,25 @@
+package ch03_scanner;
+
+import java.util.Scanner;
+
+public class Ex02_Calculator {
+
+	public static void main(String[] args) {
+		/**
+		 * 정수 2개를 각각 스캐너로 num1, num2에 입력 받아서 입력 받은 수의 합을 출력하세요.
+		 */
+		Scanner sc = new Scanner(System.in);
+		System.out.print("첫 번째 숫자를 입력하세요 : ");
+		int num1 = sc.nextInt();
+		System.out.print("두 번째 숫자를 입력하세요 : ");
+		int num2 = sc.nextInt();
+		int sum = num1 + num2;
+		System.out.println("합계 : " + sum);
+		System.out.println(num1 + "과 " + num2 + "의 합계는 " + sum + "입니다.");
+		System.out.println(num1 + " + " + num2 + " = " + sum);
+	
+		
+		
+	}
+
+}
