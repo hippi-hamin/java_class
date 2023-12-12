d680447fb2aff8b7c07509621caa18081a3be12c
hippi-hamin <9908haha@naver.com>
2023-12-12 17:29:13 +0900

    setter/getter 예제

--- src/ch10_class/ex9/Student.java
+++ src/ch10_class/ex9/Student.java
@@ -1,4 +1,73 @@
 package ch10_class.ex9;
 
+import org.w3c.dom.ls.LSOutput;
+
 public class Student {
+    /*
+        필드
+        이름(studentName)
+        학번(studentNumber)
+        학과(studentMajor)
+        전화번호(studentMobile)
+     */
+    private String studentName;
+    private int studentNumber;
+    private String studentMajor;
+    private String studentMobile;
+
+    // 각 필드의 getter/setter 메서드
+
+    // studentName setter
+    public void setStudentName(String studentName) {
+        this.studentName = studentName;
+    }
+    // studentName getter
+    public String getStudentName() {
+        return studentName;
+    }
+    // studentNumber setter
+    public void setStudentNumber(int studentNumber) {
+        this.studentNumber = studentNumber;
+    }
+    // studentNumber getter
+    public int getStudentNumber() {
+        return studentNumber;
+    }
+    // studentMajor setter
+    public void setStudentMajor(String studentMajor) {
+        this.studentMajor = studentMajor;
+    }
+    // studentMajor getter
+    public String getStudentMajor() {
+        return studentMajor;
+    }
+    // studentMobile setter
+    public void setStudentMobile(String studentMobile) {
+        this.studentMobile = studentMobile;
+    }
+    // studentMobile getter
+    public String getStudentMobile() {
+        return studentMobile;
+    }
+
+    // 기본생성자
+    public Student(){
+    }
+    // 모든 필드를 매개변수로 하는 생성자
+    public Student(String studentName, int studentNumber, String studentMajor, String studentMobile) {
+        this.studentName = studentName;
+        this.studentNumber = studentNumber;
+        this.studentMajor = studentMajor;
+        this.studentMobile = studentMobile;
+        // 호출하는 생성자를 만들어 주기 위해서 println() 메서드를 사용해주어야 함.
+        System.out.println("studentName = " + studentName + ", studentNumber = " + studentNumber + ", studentMajor = " + studentMajor + ", studentMobile = " + studentMobile);
+    }
+    // 필드에 저장된 값을 출력하는 print() 메서드
+    public void print(){
+        System.out.println(getStudentName());
+        System.out.println(getStudentNumber());
+        System.out.println(getStudentMajor());
+        System.out.println(getStudentMobile());
+    }
 }
+
--- src/ch10_class/ex9/StudentMain.java
+++ src/ch10_class/ex9/StudentMain.java
@@ -1,4 +1,22 @@
 package ch10_class.ex9;
 
+import java.io.PrintStream;
+
 public class StudentMain {
+    public static void main(String[] args) {
+        // 객체선언문 - Student라는 클래스를 객체로 사용하기 위하여 선언(공식으로 봐도 무방)
+        Student student = new Student();
+
+        // 기본생성자로 Student 클래스 객체 만들고 setter를 이용하여 필드값 채우기
+        student.setStudentName("하민");
+        student.setStudentNumber(20202020);
+        student.setStudentMajor("수학");
+        student.setStudentMobile("010-3333-3333");
+
+        // 모든 필드를 매개변수로 하는 생성자로 Student 클래스 객체 만들기
+        Student student1 = new Student("하민", 20202020, "항해학", "010-1212-1212");
+        System.out.println(student1);
+        // 각각의 객체에서 print 메서드 호출하여 필드값 출력
+        student.print();
+    }
 }
