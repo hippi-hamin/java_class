10bd7fb7ed0f36b59e4c3ef63a9c935857ea4ad8
hippi-hamin <9908haha@naver.com>
2023-12-14 15:38:49 +0900

    클래스 분리 예제 - 메인 메뉴 구성

--- src/ch12_classes/ex01/StudentMain.java
+++ src/ch12_classes/ex01/StudentMain.java
@@ -1,4 +1,32 @@
 package ch12_classes.ex01;
 
+import ch10_class.ex11.Board;
+
+import java.util.Scanner;
+
 public class StudentMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean run = true;
+        int selectNo = 0;
+
+        while (run) {
+            System.out.println("--------------------------------------------------------------");
+            System.out.println("1.method1 호출 | 2.method2 호출 | 3.method3 호출 | 4.종료");
+            System.out.println("--------------------------------------------------------------");
+            System.out.print("선택> ");
+            selectNo = sc.nextInt();
+            if(selectNo == 1){
+
+            }else if(selectNo == 2){
+
+            } else if (selectNo ==3) {
+
+            } else if (selectNo == 4) {
+                run = false;
+            }
+
+        }
+}
+
 }
