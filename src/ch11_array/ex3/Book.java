54b5d485ce18b6c6db0289fdeedbf5f4523a53f0
hippi-hamin <9908haha@naver.com>
2023-12-14 14:07:55 +0900

    ArrayList 예제 - Book

---
+++ src/ch11_array/ex3/Book.java
@@ -0,0 +1,86 @@
+package ch11_array.ex3;
+/**
+ 책 클래스
+ 필드
+ 책관리번호(id) - Long 타입
+ 책제목(bookTitle)
+ 저자(bookAuthor)
+ 가격(bookPrice)
+ 출판사(bookPublisher)
+ 필드에 대한 getter/setter
+ 생성자 2가지
+ toString method
+ */
+
+public class Book {
+    // 필드
+    private Long id;
+    private String bookTitle;
+    private String bookAuthor;
+    private int bookPrice;
+    private String bookpublisher;
+
+    // getter/setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBookTitle() {
+        return bookTitle;
+    }
+
+    public void setBookTitle(String bookTitle) {
+        this.bookTitle = bookTitle;
+    }
+
+    public String getBookAuthor() {
+        return bookAuthor;
+    }
+
+    public void setBookAuthor(String bookAuthor) {
+        this.bookAuthor = bookAuthor;
+    }
+
+    public int getBookPrice() {
+        return bookPrice;
+    }
+
+    public void setBookPrice(int bookPrice) {
+        this.bookPrice = bookPrice;
+    }
+
+    public String getBookpublisher() {
+        return bookpublisher;
+    }
+
+    public void setBookpublisher(String bookpublisher) {
+        this.bookpublisher = bookpublisher;
+    }
+
+    // 기본생성자
+    public Book(){
+    }
+    // 필드의 모든 값을 매개변수로 갖는 Book 생성자
+    public Book(Long id, String bookTitle, String bookAuthor, int bookPrice, String bookpublisher) {
+        this.id = id;
+        this.bookTitle = bookTitle;
+        this.bookAuthor = bookAuthor;
+        this.bookPrice = bookPrice;
+        this.bookpublisher = bookpublisher;
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "id=" + id +
+                ", bookTitle='" + bookTitle + '\'' +
+                ", bookAuthor='" + bookAuthor + '\'' +
+                ", bookPrice='" + bookPrice + '\'' +
+                ", bookpublisher='" + bookpublisher + '\'' +
+                '}';
+    }
+}
---
+++ src/ch11_array/ex3/BookMain.java
@@ -0,0 +1,66 @@
+package ch11_array.ex3;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BookMain {
+    // Book 객체를 담기위한 bookList 객체 선언
+
+    // 기본생성자로 Book 객체(book1) 생성 후 bookList에 추가
+
+    // 매개변수 생성자로 Book 객체(book2) 생성 후 bookList에 추가
+
+    // for을 이용하여 bookList의 전체 데이터 출력
+
+    // for each를 이용하여 bookList의 전체 데이터 출력
+
+    // list의 0번 인덱스에 담긴 책의 가격을 50000으로 변경
+
+    // list의 1번 인덱스에 담긴 출판사의 제목을 "안녕출판사"로 변경
+
+    // for를 이용하여 bookList의 전체 데이터 출력
+
+    // for each를 이용하여 bookList의 전체 데이터 출력
+
+    public static void main(String[] args) {
+        // bookList에 Book 객체 담기
+        List<Book> bookList = new ArrayList<>();
+        Book book1 = new Book();
+        // 기본생성자로 객체(book1)생성
+        book1.setId(1L);
+        book1.setBookTitle("어린왕자");
+        book1.setBookAuthor("앙투안 드 생텍쥐페리");
+        book1.setBookPrice(10620);
+        book1.setBookpublisher("열린책들");
+        // bookList 라는 ArrayList 에 book1추가
+        bookList.add(book1);
+        // 매개변수 생성자 Book 객체(book2) 생성
+        Book book2 = new Book(1L, "내가 생각한 인생이 아니야", "류시화", 16200, "수오서재");
+        // bookList 에 book2 추가
+        bookList.add(book2);
+        // for문을 써서 bookList 전체 데이터 값 출력
+        for (int i = 0; i < bookList.size(); i++) {
+            System.out.println("book" + (i+1) + " = " + bookList.get(i));
+        }
+        // for each를 사용하여 bookList에 전체 데이터 출력
+        for (Book a : bookList) {
+            System.out.println("book = " + a);
+        }
+        // List의 0번 인덱스에 담긴 책의 가격을 50000으로 변경
+        bookList.get(0).setBookPrice(50000);
+        // 변경된 값 확인
+        System.out.println("book1 price = " + bookList.get(0).getBookPrice());
+        // List의 1번 인덱스에 담긴 출판사의 제목을 "안녕출판사"로 변경
+        bookList.get(1).setBookpublisher("안녕출판사");
+        // 변경된 값 확인
+        System.out.println("book2 publisher = " + bookList.get(1).getBookpublisher());
+        // for 로 bookList 전체 데이터 출력
+        for (int i = 0; i < bookList.size(); i++) {
+            System.out.println("book" + (i+1) + " = " + bookList.get(i));
+        }
+        // for each를 이용하여 bookList의 전체 데이터 출력
+        for (Book a : bookList) {
+            System.out.println("book = " + a);
+        }
+    }
+}
