16176b0fa58dcd753b0ede4b77c15f5ae3cdd023
hippi-hamin <9908haha@naver.com>
2023-12-19 08:54:24 +0900

    자바 게시판 프로젝트 - BoardDTO 생성

--- src/ch12_classes/ex04/BoardDTO.java
+++ src/ch12_classes/ex04/BoardDTO.java
@@ -1,4 +1,80 @@
 package ch12_classes.ex04;
 
 public class BoardDTO {
+    //필드
+    private Long id; // 글번호
+    private String boardTitles; // 제목
+    private String boardWriter; // 작성자
+    private String boardContents; // 내용
+    private String boardPass; // 비밀번호
+
+    // getter/setter
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getBoardTitles() {
+        return boardTitles;
+    }
+
+    public void setBoardTitles(String boardTitles) {
+        this.boardTitles = boardTitles;
+    }
+
+    public String getBoardWriter() {
+        return boardWriter;
+    }
+
+    public void setBoardWriter(String boardWriter) {
+        this.boardWriter = boardWriter;
+    }
+
+    public String getBoardContents() {
+        return boardContents;
+    }
+
+    public void setBoardContents(String boardContents) {
+        this.boardContents = boardContents;
+    }
+
+    public String getBoardPass() {
+        return boardPass;
+    }
+
+    public void setBoardPass(String boardPass) {
+        this.boardPass = boardPass;
+    }
+
+    // 기본생성자
+    public BoardDTO(){
+
+    }
+
+    // idValue 절댓값으로 삼아 값을 중가시키는 메서드
+    public static Long idValue = 1L;
+    // 필드 값을 매개변수로 갖는 기본 생성자
+    public BoardDTO(String boardTitles, String boardWriter, String boardContents, String boardPass) {
+        this.id = idValue++;
+        this.boardTitles = boardTitles;
+        this.boardWriter = boardWriter;
+        this.boardContents = boardContents;
+        this.boardPass = boardPass;
+    }
+
+    // toString
+
+    @Override
+    public String toString() {
+        return "BoardDTO{" +
+                "id=" + id +
+                ", boardTitles='" + boardTitles + '\'' +
+                ", boardWriter='" + boardWriter + '\'' +
+                ", boardContents='" + boardContents + '\'' +
+                ", boardPass='" + boardPass + '\'' +
+                '}';
+    }
 }
--- src/ch12_classes/ex04/BoardMain.java
+++ src/ch12_classes/ex04/BoardMain.java
@@ -1,4 +1,41 @@
 package ch12_classes.ex04;
 
+import java.util.Scanner;
+
 public class BoardMain {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        boolean run = true;
+        int selectNo = 0;
+        BoardService boardService = new BoardService();
+
+        while(run){
+            System.out.println("-----------------------------------------------------------------------------------------");
+            System.out.println("1.글작성 | 2.글목록 | 3.글조회 | 4.글수정 | 5.글삭제 | 6.검색 | 0.종료");
+            System.out.println("-----------------------------------------------------------------------------------------");
+            System.out.print("선택> ");
+            selectNo = sc.nextInt();
+            if (selectNo == 1) {
+                // 호출하는 문장 작성
+
+            } else if (selectNo == 2) {
+                // 호출하는 문장 작성
+
+            } else if (selectNo == 3) {
+                // 호출하는 문장 작성
+
+            } else if (selectNo == 4) {
+                // 호출하는 문장 작성
+
+            } else if (selectNo == 5) {
+                // 호출하는 문장 작성
+
+            } else if (selectNo == 6) {
+                // 호출하는 문장 작성
+
+            } else if (selectNo == 0) {
+                run = false;
+            }
+        }
+    }
 }
