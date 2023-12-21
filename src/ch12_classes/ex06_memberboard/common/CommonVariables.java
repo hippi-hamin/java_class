2fb7e37208376a5aa36b88d35004bc02a9e9b46d
hippi-hamin <9908haha@naver.com>
2023-12-21 14:32:47 +0900

    회원제 게시판 프로젝트 - 회원가입

--- src/ch12_classes/ex06_memberboard/common/CommonVariables.java
+++ src/ch12_classes/ex06_memberboard/common/CommonVariables.java
@@ -1,4 +1,41 @@
 package ch12_classes.ex06_memberboard.common;
 
+import ch12_classes.ex06_memberboard.service.BoardService;
+import ch12_classes.ex06_memberboard.service.MemberService;
+
+import java.util.Scanner;
+
 public class CommonVariables {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        MemberService memberService = new MemberService();
+        BoardService boardService = new BoardService();
+        boolean run = true;
+        int selectNo = 0;
+        while (run) {
+            System.out.println("-----------------------------------------------------------------------------------------");
+            System.out.println("1.회원가입 | 2.로그인 | 3.회원목록 | 4.회원수정 | 5.회원탈퇴 | 6.로그아웃 | 7.게시판메뉴 | 0.종료");
+            System.out.println("-----------------------------------------------------------------------------------------");
+            System.out.print("선택>  ");
+            selectNo = sc.nextInt();
+            if (selectNo == 1) {
+                memberService.save();
+            } else if (selectNo == 2) {
+
+            } else if (selectNo == 3) {
+
+            } else if (selectNo == 4) {
+
+            } else if (selectNo == 5) {
+
+            } else if (selectNo == 6) {
+
+            } else if (selectNo == 7) {
+
+            } else if (selectNo == 0) {
+                System.out.println("종료");
+                run = false;
+            }
+        }
+    }
 }
--- src/ch12_classes/ex06_memberboard/repository/MemberRepository.java
+++ src/ch12_classes/ex06_memberboard/repository/MemberRepository.java
@@ -1,4 +1,14 @@
 package ch12_classes.ex06_memberboard.repository;
 
+import ch12_classes.ex06_memberboard.dto.MemberDTO;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class MemberRepository {
+    private static List<MemberDTO> memberDTOList = new ArrayList<>();
+
+    public boolean save(MemberDTO memberDTO){
+        return memberDTOList.add(memberDTO);
+    }
 }
